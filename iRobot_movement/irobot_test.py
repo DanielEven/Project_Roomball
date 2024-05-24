89cd8b3cd9c9faa4f49c469153f7c6dd705f91ec
omrisdeor <omrisdeor@mail.tau.ac.il>
2024-05-24 13:50:53 +0300

    changed iRobot code

--- iRobot_movement/irobot_test.py
+++ iRobot_movement/irobot_test.py
@@ -1,38 +1,29 @@
-#
-# Licensed under 3-Clause BSD license available in the License file. Copyright (c) 2021-2023 iRobot Corporation. All rights reserved.
-#
-
-# Want to draw a square?
-
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
-from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
-from irobot_edu_sdk.music import Note
-import time
+from irobot_edu_sdk.robots import event, Create3
 
-robot = Root(Bluetooth())
-#robot = Create3(Bluetooth())
+robot = Create3(Bluetooth())
 
-notes = "eee eee egcde fff ffee eeggfdc "
-beats = [2,2,3,1, 2,2,3,1, 2,2,3,1,4,4, 2,2,3,0, 1,2,2,2,0, 1,1,2,2,2,2,4,4]
-songLength = len(notes)
-tempo = 0.1
 
 @event(robot.when_play)
 async def draw_square(robot):
-    # await robot.navigate_to(16,16)  # Will have no effect on Create 3.
+    # move robot until it is close to an object
+    await robot.set_wheel_speeds(100, 100)
+    while max((await robot.get_ir_proximity()).sensors) < 80:
+        pass
+    await robot.set_wheel_speeds(0, 0)
 
-    # The "_" means that we are not using the temporal variable to get any value when iterating.
-    # So the purpose of this "for" loop is just to repeat 4 times the actions inside it:
-    
-    await robot.move(50)  # cm
-    #await robot.turn_left(90)  # deg
-    await robot.move(-50)  # Will have no effect on Create 3.
-
-    for i in range(songLength):
-        duration = beats[i] * tempo
-        if (notes[i] == ' '):
-            time.sleep(duration)
+    # set rgb based on distance to closest object
+    while True:
+        ir_sensors = (await robot.get_ir_proximity()).sensors
+        print(ir_sensors)
+        if max(ir_sensors) > 1500:
+            await robot.set_lights_on_rgb(0, 0, 255)
+        elif max(ir_sensors) > 500:
+            await robot.set_lights_on_rgb(0, 255, 0)
+        elif max(ir_sensors) > 100:
+            await robot.set_lights_on_rgb(255, 0, 0)
         else:
-            await robot.play_note(eval(f"Note.{notes[i].capitalize()}4"), duration)
+            await robot.set_lights_off()
+
 
 robot.play()
