a7e6b0f47c7e87494365f5f027b08ff475ac3851
Daniel Even <danieleven@mail.tau.ac.il>
2024-06-04 15:17:21 +0300

    Improved parsing, added close function, added description.

--- myCobot_server/myCobot_server.py
+++ myCobot_server/myCobot_server.py
@@ -5,16 +5,22 @@ import re
 import argparse
 
 DEFAULT_IP = '192.168.0.134'
+DEFAULT_PORT = 12355
 
 app = Flask(__name__)
 
 
 def parse_arg(arg: str):
+    """
+    Parse a single argument from string format to the appropriate type.
+    Supports int, float, tuple of int/float, and str.
+    """
     if arg.startswith('(') and arg.endswith(')'):
-        if '.' in arg:
-            return tuple(float(x) for x in arg[1:-1].split(","))
+        elements = arg[1:-1].split(",")
+        if all(element.replace('.', '', 1).isdigit() for element in elements):
+            return tuple(float(x) if '.' in x else int(x) for x in elements)
         else:
-            return tuple(int(x) for x in arg[1:-1].split(","))
+            return tuple(elements)
     elif arg.isdigit():
         return int(arg)
     elif arg.replace('.', '', 1).isdigit():
@@ -23,30 +29,47 @@ def parse_arg(arg: str):
         return arg
 
 
-def parse_args(args):
+def parse_args(args: str):
+    """
+    Parse a comma-separated string of arguments into a list of parsed arguments.
+    """
     args = re.split(r',(?![^()]*\))', args)
     return [parse_arg(arg) for arg in args]
 
 
-@app.route('/call/<function_name>')
+@app.route('/call/<function_name>', methods=['GET'])
 def call_function(function_name):
+    """
+    Endpoint to call a function of the MyCobot class by name with optional arguments.
+    """
     func = getattr(MyCobot, function_name, None)
     if func is None or not inspect.isroutine(func):
         return jsonify({'error': f'No such function: {function_name}'}), 404
 
     args = request.args.get('args', '')
     try:
-        result = func(mc, *parse_args(args))
+        parsed_args = parse_args(args)
+        result = func(mc, *parsed_args)
         return jsonify({'result': result})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
 
+@app.route('/close_server', methods=['GET'])
+def close_server():
+    """
+    Close the flask server and disconnect the myCobot instance.
+    """
+    mc.close()
+    exit(0)
+
 def get_arguments():
+    """
+    Parse command line arguments for the Flask server.
+    """
     parser = argparse.ArgumentParser(description='myCobot 3 Command Server.🚀🔥')
-    parser.add_argument('--host', type=str, help='host ip',
-                        default=DEFAULT_IP)
-    parser.add_argument('--port', type=int, help='port number', default=12355)
+    parser.add_argument('--host', type=str, help='host ip', default=DEFAULT_IP)
+    parser.add_argument('--port', type=int, help='port number', default=DEFAULT_PORT)
     return parser.parse_args()
 
 
