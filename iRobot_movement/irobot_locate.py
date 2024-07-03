1c620f7aaad6d2035cf3592cae19013261f43984
Daniel Even <danieleven@mail.tau.ac.il>
2024-07-03 14:35:23 +0300

    Locating the obstacle and moving towards it

---
+++ iRobot_movement/irobot_locate.py
@@ -0,0 +1,40 @@
+import argparse
+from irobot_edu_sdk.backend.bluetooth import Bluetooth
+from irobot_edu_sdk.robots import event, Create3
+
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from myCobot_client.myCobot_client import call_cobot_function
+
+COBOT_IP = "192.168.0.134"
+COBOT_PORT = 12335 
+
+DRIVING_SPEED = 10
+DETECTION_DISTANCE_THRESHOLD = 200
+ROTATION_SPEED = 1
+DISTANCE_FROM_OBSTACLE = [10, 5]
+
+
+robot = Create3(Bluetooth())
+
+@event(robot.when_touched, [True, False])
+async def locate_item(robot):
+
+    # Spinning until the robot detects an obstacle
+    await robot.set_wheel_speeds(ROTATION_SPEED, -ROTATION_SPEED)
+    call_cobot_function(COBOT_IP, COBOT_PORT, "wait_for_obstacle", DETECTION_DISTANCE_THRESHOLD, 1, custom=True) # 1 = the sensor to wait for
+    await robot.set_wheel_speeds(0, 0)
+
+    # Read the obstacle distance and move towards it + fixing distance
+    for dist in  DISTANCE_FROM_OBSTACLE:
+        res = call_cobot_function(COBOT_IP, COBOT_PORT, "get_ultrasonic_sensors")
+        await robot.move(res[1] - dist) # Move the robot to near the obstacle
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='mycobot server')
+    parser.add_argument('--host', type=str, help='host ip', default=COBOT_IP)
+    parser.add_argument('--port', type=int, help='port number', default=COBOT_PORT)
+    args = parser.parse_args()
+
