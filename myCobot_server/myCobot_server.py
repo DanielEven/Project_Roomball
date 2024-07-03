4c0ea6f8448ddf9fee3bd43d0d40515e771cbcc3
omrisdeor <omrisdeor@mail.tau.ac.il>
2024-07-03 14:36:36 +0300

    added wait on specified sensor

--- myCobot_server/myCobot_server.py
+++ myCobot_server/myCobot_server.py
@@ -96,6 +96,9 @@ def wait_for_obstacle():
     args = request.args.get('args', '')
     parsed_args = parse_args(args)
     distance = parsed_args[0]
+    sensor = None
+    if len(parsed_args) == 2:
+        sensor = parsed_args[1]
     logger.info(f"Calling custom function wait_for_obstacle with arguments {parsed_args}")
 
     if ser is None:
@@ -110,7 +113,10 @@ def wait_for_obstacle():
         while not line.startswith("Distances:"):
             line = ser.readline().decode()
         sensor_values =  [float(x) for x in line.split()[1:]]
-        while min(sensor_values) > distance:
+        print("-----------------------------------")
+        print(min(sensor_values) if sensor is None else sensor_values[sensor])
+        print("-----------------------------------")
+        while (min(sensor_values) if sensor is None else sensor_values[sensor]) > distance:
             line = ser.readline().decode()
             while not line.startswith("Distances:"):
                 line = ser.readline().decode()
